4a44599a83eb4383e430d47af27d3b59d840ec32
agwenchez <agweraenock76@gmail.com>
2020-07-20 08:19:39 -0400

    some minor bug fixes

--- micro/routes/sendmail.js
+++ micro/routes/sendmail.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
-const { default: Axios } = require('axios');
-// const e = require('express');
+// const { default: Axios } = require('axios');
+const Messages = require('../models/Pre-msg');;
 
 const transporter = nodemailer.createTransport(
     sendgridTransport({
         auth: {
-            api_key: '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'
+            api_key: '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'
         }
     })
 );
@@ -24,49 +24,61 @@ router.get('/', (req, res) => res.json({
 // sending mail
 router.post('/sendmail', (req, res) => {
     const {
+        header,
         name,
         email,
-        header,
-        message
+       
+        // message
     } = req.body;
 
     
 
-    Axios.get('http://localhost:5000/artists/artists').then( response=>{
+    // Axios.get('http://localhost:5000/artists/artists').then( response=>{
 
-        const data = response.data;
-        console.log(data);
+    //     const data = response.data;
+    //     console.log(data);
         
-        const sender = data.map (item => item.email);
-        console.log(sender);
+    //     const sender = data.map (item => item.email);
+    //     console.log(sender);
 
-        const found = sender.find( element => element === email);
-        console.log(found);
+    //     const found = sender.find( element => element === email);
+    //     console.log(found);
         
-        // const found = sender.find(element => element === email);
+    //     // const found = sender.find(element => element === email);
 
-        if(!found){
-            return res.status(400).json({
-                msg: "Kindly provide an email of a valid user"
-            })
-        }else{
+    //     if(!found){
+    //         return res.status(400).json({
+    //             msg: "Kindly provide an email of a valid user"
+    //         })
+    //     }else{
 
-           return res.status(200).send(
+
+        Messages.findOne({name:header}).then( result =>{
+
+            // console.log(result);
+            // res.status(200).send(result);
+            return res.status(200).send(
                 transporter.sendMail({
-                    to: found,
+                    to: email,
                     from: 'agwenchez254@bcydn.org',
-                    subject:`${header}`,
+                    subject:`${result.name}`,
                     html:` <p>Dear ${name},
-                            ${message}</p>`
+                            ${result.message}</p>`
                 })
                )
-        }
+        })
+
+
+
+
+        
+        
 
        
    
      
 
-    }); 
+ 
 
 });
 
